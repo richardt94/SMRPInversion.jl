dd5e8f2163ac8c14b8e39b61bf16c541daf869e4
Anandaroop Ray <a2ray@ucsd.edu>
2025-12-02 15:15:07 +1100

    WIP: real data sounding

--- UDF_example/01_load_data.jl
+++ UDF_example/01_load_data.jl
@@ -26,9 +26,9 @@ extendfrac, dz = 1.028, 1.
 zall, znall, zboundaries = transD_GP.setupz(zstart, extendfrac, dz=dz, n=70, showplot=true)
 ##
 linearsat = true
-amponly = false
+amponly = true
 mult = false
-phaserev = true
+phaserev = false
 ##
 phaserev && (ϕ=-ϕ)
 F = SMRPInversion.MRSForward_square(L, zboundaries, q[:], inclination, θ, Be, σt)
